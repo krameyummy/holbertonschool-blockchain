a9bf33cd0c8bb433dac2263c8598ecdef47e24d1
NourElHouda-Sarmout <2129@holbertonschool.com>
2020-12-07 17:24:33 +0200

    Fix error

--- crypto/ec_save.c
+++ crypto/ec_save.c
@@ -1,41 +1,37 @@
 #include "hblk_crypto.h"
 
 /**
- * ec_save - saves public/private keys in PEM format
- * @key: pointer to EC_KEY struct with key pair
- * @folder: the folder to save, created if need be
- * Return: 1 on success else 0
+ * ec_save - save pub/private key pair into a file
+ * @key: key pair
+ * @folder: directory where to store the file
+ * Return: 1 on success, 0 on failure
  */
 int ec_save(EC_KEY *key, char const *folder)
 {
+	char file[512] = {0};
 	FILE *fp;
-	char path[128] = {0};
+	struct stat st = {0};
 
 	if (!key || !folder)
 		return (0);
-	mkdir(folder, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
-
-	sprintf(path, "%s/" PRI_FILENAME, folder);
-	fp = fopen(path, "w");
+	if (stat(folder, &st) == -1)
+	{
+		if (mkdir(folder, 0700) == -1)
+			return (0);
+	}
+	sprintf(file, "%s/%s", folder, PRI_FILENAME);
+	fp = fopen(file, "w");
 	if (!fp)
 		return (0);
 	if (!PEM_write_ECPrivateKey(fp, key, NULL, NULL, 0, NULL, NULL))
-	{
-		/*fclose(fp);*/
-		/*return (0);*/
-	}
+		return (0);
 	fclose(fp);
-
-	sprintf(path, "%s/" PUB_FILENAME, folder);
-	fp = fopen(path, "w");
+	sprintf(file, "%s/%s", folder, PUB_FILENAME);
+	fp = fopen(file, "w");
 	if (!fp)
 		return (0);
 	if (!PEM_write_EC_PUBKEY(fp, key))
-	{
-		fclose(fp);
 		return (0);
-	}
 	fclose(fp);
-
 	return (1);
 }
