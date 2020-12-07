8bf5caef40ba505a830c047cd57c4ccf992d3e1b
NourElHouda-Sarmout <2129@holbertonschool.com>
2020-12-07 18:12:02 +0200

    Fix error

--- crypto/ec_load.c
+++ crypto/ec_load.c
@@ -16,14 +16,14 @@ EC_KEY *ec_load(char const *folder)
 		return (NULL);
 	if (stat(folder, &st) == -1)
 		return (NULL);
-	sprintf(file, "./%s/%s", folder, PUB_FILE);
+	sprintf(file, "./%s/%s", folder, PUB_FILENAME);
 	fp = fopen(file, "r");
 	if (!fp)
 		return (NULL);
 	if (!PEM_read_EC_PUBKEY(fp, &key, NULL, NULL))
 		return (NULL);
 	fclose(fp);
-	sprintf(file, "./%s/%s", folder, PRV_FILE);
+	sprintf(file, "./%s/%s", folder, PRI_FILENAME);
 	fp = fopen(file, "r");
 	if (!fp)
 		return (NULL);
