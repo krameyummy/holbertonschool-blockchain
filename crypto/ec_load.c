abb43af02fdde4c78f8c99db5b7c0d1484973cf5
NourElHouda-Sarmout <2129@holbertonschool.com>
2020-12-07 17:13:56 +0200

    Fix error

--- crypto/ec_load.c
+++ crypto/ec_load.c
@@ -1,43 +1,33 @@
 #include "hblk_crypto.h"
 
 /**
- * ec_load - loads an existing EC key pair on the disk
+ * ec_load - load an EC key pair from the disk
+ * @folder: path to folder from which to load the keys
  *
- * @folder: path to the folder from which to load the keys
- *
- * Return: pointer to EC key pair, or NULL on failure
+ * Return: pointer to created EC key pair, NULL on error
  */
 EC_KEY *ec_load(char const *folder)
 {
-	char buffer[BUFSIZ];
 	EC_KEY *key = NULL;
+	char buf[BUFSIZ];
 	FILE *fp;
 
-	if (!folder)
+	if (!folder || strlen(folder) + strlen(PUB_FILENAME) > BUFSIZ)
 		return (NULL);
 
-	sprintf(buffer, "%s/%s",  folder, PUB_FILENAME);
-	fp = fopen(buffer, "r");
+	sprintf(buf, "%s/%s", folder, PUB_FILENAME);
+	fp = fopen(buf, "r");
 	if (!fp)
 		return (NULL);
-
 	if (!PEM_read_EC_PUBKEY(fp, &key, NULL, NULL))
-	{
-		fclose(fp);
-		return (NULL);
-	}
-	fclose(fp);
-
-	sprintf(buffer, "%s/%s", folder, PRI_FILENAME);
-	fp = fopen(buffer, "r");
+		goto out;
+	sprintf(buf, "%s/%s", folder, PRI_FILENAME);
+	fp = fopen(buf, "r");
 	if (!fp)
 		return (NULL);
-
 	if (!PEM_read_ECPrivateKey(fp, &key, NULL, NULL))
-	{
-		fclose(fp);
-		return (NULL);
-	}
+		goto out;
+out:
 	fclose(fp);
 	return (key);
 }
