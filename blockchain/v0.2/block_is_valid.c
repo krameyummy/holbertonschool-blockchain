b047e5145f5ff988a68d161a8f66313943975185
NourElHouda-Sarmout <2129@holbertonschool.com>
2020-12-23 20:57:08 +0200

    Solve Task 2

--- blockchain/v0.2/block_is_valid.c
+++ blockchain/v0.2/block_is_valid.c
@@ -18,30 +18,12 @@ static int verify_genesis_block(block_t const *block)
 }
 
 /**
- * verify_transactions - verify each transaction
- * @node: current node
- * @idx: index of @node
- * @all_unspent: list of unspent transactions
- * Return: 0 if transaction is valid, 1 otherwise
- */
-int verify_transactions(llist_node_t node, unsigned int idx, void *all_unspent)
-{
-	if (idx == 0)
-		return (0);
-	if (!transaction_is_valid(node, all_unspent))
-		return (1);
-	return (0);
-}
-
-/**
  * verify_blocks - a function that verifies that a Block is valid
  * @block: block to be verified
  * @prev_block: previous block
- * @all_unspent: unspent transactions
  * Return: -1 if block is not valid, 0 if block is valid
  */
-static int verify_blocks(block_t const *block,
-			 block_t const *prev_block, llist_t *all_unspent)
+static int verify_blocks(block_t const *block, block_t const *prev_block)
 {
 	uint8_t block_sha[SHA256_DIGEST_LENGTH];
 	uint8_t prev_sha[SHA256_DIGEST_LENGTH];
@@ -57,13 +39,6 @@ static int verify_blocks(block_t const *block,
 		return (-1);
 	if (block->data.len > BLOCKCHAIN_DATA_MAX)
 		return (-1);
-	if (llist_size(block->transactions) < 1)
-		return (-1);
-	if (!coinbase_is_valid(llist_get_head(block->transactions),
-			       block->info.index))
-		return (-1);
-	if (llist_for_each(block->transactions, verify_transactions, all_unspent))
-		return (-1);
 	return (0);
 }
 
@@ -71,11 +46,9 @@ static int verify_blocks(block_t const *block,
  * block_is_valid - a function that verifies that a Block is valid
  * @block: block to be verified
  * @prev_block: previous block
- * @all_unspent: unspent transactions
  * Return: -1 if block is not valid, 0 if block is valid
  */
-int block_is_valid(block_t const *block, block_t const *prev_block,
-		   llist_t *all_unspent)
+int block_is_valid(block_t const *block, block_t const *prev_block)
 {
 	if (!block)
 		return (-1);
@@ -83,5 +56,5 @@ int block_is_valid(block_t const *block, block_t const *prev_block,
 		return (-1);
 	if (!prev_block)
 		return (verify_genesis_block(block));
-	return (verify_blocks(block, prev_block, all_unspent));
+	return (verify_blocks(block, prev_block));
 }
