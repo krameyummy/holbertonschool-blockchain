f73b0bced9323fa9f3d0338d174d42a729087631
NourElHouda-Sarmout <2129@holbertonschool.com>
2021-01-10 22:20:09 +0200

    update header file

--- blockchain/v0.3/transaction/transaction.h
+++ blockchain/v0.3/transaction/transaction.h
@@ -111,4 +111,7 @@ void transaction_destroy(transaction_t *transaction);
 
 int filter_unspent(llist_node_t node, unsigned int idx, void *args);
 
+llist_t *update_unspent(llist_t *transactions,
+			uint8_t block_hash[SHA256_DIGEST_LENGTH], llist_t *all_unspent);
+
 #endif  /* __TRANSACTION_H_ */
